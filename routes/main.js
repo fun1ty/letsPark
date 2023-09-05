2f4d6dba16cd83bbf28bb7292264e10b7a225a8e
ddomiunnie <sjookim417@naver.com>
2023-09-05 13:59:29 +0900

    feat: 회원 관리 기능

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,22 @@
-const express = require("express");
-const controller = require("../controller/Cmain");
+const express = require('express');
+const controller = require('../controller/Cmain');
 const router = express.Router();
 
-router.get("/", controller.main);
+router.get('/', controller.main);
+
+router.get('/login', controller.login); //로그인
+router.post('/login', controller.postLogin);
+
+router.get('/signup', controller.signup); //회원가입
+router.post('/signup', controller.postSignup);
+
+router.get('/verify', controller.verify); //전화번호 인증
+
+router.get('/:userid/:nickname/success', controller.success); //회원가입 성공
+
+router.get('/user/:userid/edit', controller.edit); //회원정보 수정
+router.patch('/edit', controller.editProfile);
+
+router.delete('/drop', controller.drop); //회원 탈퇴
 
 module.exports = router;
