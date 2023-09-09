d29da04f610622b7c7ac6824c888e7ff55cf105a
suhyuns22 <138412534+suhyuns22@users.noreply.github.com>
2023-09-09 11:52:18 +0900

    검색 완료

--- routes/main.js
+++ routes/main.js
@@ -6,5 +6,6 @@ router.get("/", controller.main);
 router.get("/chat", controller.chat);
 
 router.get("/ppdb", controller.ppdb);
+router.get("/parking", controller.parking);
 
 module.exports = router;
