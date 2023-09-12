a1e86fc2b8fbafaf55ebcb8280c58e4b5c6b30e9
MiRi <116661333+fun1ty@users.noreply.github.com>
2023-09-12 10:28:48 +0900

    Update JwtVerifyToken.js

--- utils/JwtVerifyToken.js
+++ utils/JwtVerifyToken.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const env = process.env;
@@ -7,15 +7,7 @@ const SECRETKEY = env.SECRETKEY;
 exports.verifyToken = (token) => {
   console.log("토큰검증 실행");
   console.log(token);
-=======
-const jwt = require('jsonwebtoken');
-const env = process.env;
-require('dotenv').config();
-const SECRETKEY = env.SECRETKEY;
 
-exports.verifyToken = (token) => {
-  console.log('토큰검증 실행');
->>>>>>> 1ec4eeab4d8462f08b20f9421001581f35d01fee
   let userId;
   return new Promise((resolve, reject) => {
     jwt.verify(token, SECRETKEY, (err, decoded) => {
