a2fcb791b15a2225822f4d665b010877ce69e7b9
ddomiunnie <sjookim417@naver.com>
2023-09-12 09:15:48 +0900

    feat: 프로필 사진 추가

--- routes/main.js
+++ routes/main.js
@@ -2,16 +2,11 @@ const express = require('express');
 const controller = require('../controller/Cmain');
 const router = express.Router();
 
-
-router.post("/", controller.getInfo);
-
-
-const jwt = require('jsonwebtoken');
+router.post('/', controller.getInfo);
 
 router.get('/', controller.main);
 router.get('/chat', controller.chat);
 
 router.get('/ppdb', controller.ppdb);
-
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller/Cuser');
+const upload = require('../utils/S3upload');
 
 router.get('/login', controller.login); //로그인
 router.post('/login', controller.postLogin);
 
 router.get('/signup', controller.signup); //회원가입
-router.post('/signup', controller.postSignup);
+router.post('/signup', upload.single('profile'), controller.postSignup);
 
 router.get('/:userid/:nickname/success', controller.success); //회원가입 성공
 
